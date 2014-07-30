44f34ed340a5a4f381401f0bad45a9867709de79
Frank J. Gómez <frank@ginkgostreet.com>
2014-07-30 16:24:52 -0400

    Workaround for CiviCRM core issue where case XML fails to load if case is created via the API.

--- CRM/Enrollment/BAO/Enrollment.php
+++ CRM/Enrollment/BAO/Enrollment.php
@@ -111,6 +111,7 @@ class CRM_Enrollment_BAO_Enrollment {
     if (empty($this->case)) {
       $result = civicrm_api3('Case', 'create', array(
         'case_type_id' => self::$policy_case_type_id,
+        'case_type' => self::POLICY_CASE_TYPE,
         'contact_id' => $this->client_id,
         'start_date' => date('Y-m-d'),
         'subject' => ts('Workplace Policy for %1', array(1 => $this->client_display_name, 'domain' => 'org.drugfreepa.enrollment')),
